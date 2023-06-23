430ac87f2ea63b66a1792c02fbcc46e4bd372390
Ogedegbe O Eric <ohimaigold@gmail.com>
2023-06-22 23:36:42 -0700

    0x0A. Configuration management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#Using puppet ensure file exist and check requirements
+
+file {'/tmp/school':
+        ensure  => 'present',
+        owner   => 'www-data',
+        group   => 'www-data',
+        mode    => '0744',
+        content => 'I love Puppet'
+}
